52aeade05a7abcab77ea08e1783c8c54d7cf1b31
Tamerlan <t.dukov79@gmail.com>
2026-02-25 23:14:47 +0300

    feat: add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 import 'data/mock_repository.dart';
+import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 
-Stream<int> co() async* {
-  yield 1;
-}
-
-void main() {
-  StreamController controller = StreamController();
-  final stream = co().asBroadcastStream();
-  StreamSubscription sub = stream.listen((data) {});
-  StreamSubscription subb = stream.listen((data) {});
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
